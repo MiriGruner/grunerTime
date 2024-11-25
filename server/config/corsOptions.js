9e44236b839fb4ac4688d39f4a50585cb24cc128
MiriGruner <144209926+MiriGruner@users.noreply.github.com>
2024-11-25 13:50:35 +0200

    Update corsOptions.js

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,8 +1,9 @@
 const allowedOrigins = [
+    'https://grunertime.onrender.com',
     'http://localhost:3000',
     'http://localhost:5500',
-    'http://localhost:8000',
-    'https://grunertime.onrender.com'
+    'http://localhost:8000'
+    
 ]
 
 const corsOptions = {
